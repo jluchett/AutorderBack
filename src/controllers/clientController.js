4cc021940e0a5d4830d489d5af95c221e61ad8b4
jluchett <jluchett@hotmail.com>
2023-07-24 23:47:41 -0500

    Added update and delete of client

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -50,7 +50,65 @@ const createClient = async (req, res) => {
   }
 };
 
+const updateClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, telefono, email } = req.body;
+
+    const query =
+      "UPDATE clientes SET nombre = $2, telefono = $3, email = $4 WHERE id = $1";
+    const values = [id, nombre, telefono, email];
+
+    const result = await db.query(query, values);
+
+    if (result.rowCount === 0) {
+      // La consulta no modificó ninguna fila en la base de datos
+      return res.status(404).json({
+        message: "Cliente no encontrado",
+        succes: false,
+      });
+    }
+    // Devolver la respuesta con los datos actualizados
+    return res.status(200).json({
+      message: "Datos del cliente actualizados",
+      succes: true,
+    });
+  } catch (error) {
+    console.error("Eror al actualizar info del cliente", error);
+    res.status(500).json({
+      message: "Eror al actualizar info del cliente",
+      succes: false,
+    });
+  }
+};
+
+const deleteClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = "Delete FROM clientes WHERE id = $1";
+    const result = await db.query(query, [id]);
+    if (result.rowCount === 0) {
+      return res.status(400).json({
+        message: "No se ha eliminado cliente",
+        succes: false,
+      });
+    }
+    return res.status(201).json({
+      message: "Cliente eliminado con exito",
+      succes: true,
+    });
+  } catch (error) {
+    console.error("Error al eliminar cliente", error);
+    res.status(500).json({
+      message: "Eror al eliminar cliente",
+      succes: false,
+    });
+  }
+};
+
 module.exports = {
   getClients,
   createClient,
+  updateClient,
+  deleteClient,
 };
--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -4,5 +4,7 @@ const clientController = require("../controllers/clientController");
 
 router.get("/", clientController.getClients);
 router.post("/add", clientController.createClient)
+router.put("/update/:id", clientController.updateClient)
+router.delete("/delete/:id", clientController.deleteClient)
 
 module.exports = router;
