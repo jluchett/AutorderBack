cad11ec80a860e74d4cd13c06c73f96e169b779e
jluchett <jluchett@hotmail.com>
2023-08-07 23:29:01 -0500

    starting functionalities orders

---
+++ src/controllers/orderController.js
@@ -0,0 +1,11 @@
+const getOrders = () =>{}
+const createOrder = () =>{}
+const updateOrder = () =>{}
+const deleteOrder = () =>{}
+
+module.exports = {
+    getOrders,
+    createOrder,
+    updateOrder,
+    deleteOrder,
+}
---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/orderController");
+
+router.get();
+router.post();
+router.put();
+router.patch();
+router.delete();
+
+module.exports = router;
