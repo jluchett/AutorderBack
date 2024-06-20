b4b90434d23aa4304ccbf9616f6f8bc9aa1e1fd7
jluchett <jluchett@hotmail.com>
2024-06-19 22:45:34 -0500

    Added close conexion in ds.js

--- src/database/db.js
+++ src/database/db.js
@@ -11,16 +11,27 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+// Manejo del cierre de la conexión
+pool.on('error', (err) => {
+  console.error('Error en el pool de conexiones:', err);
+  process.exit(-1);
+});
+
 // Exporta el objeto de conexión para usarlo en otros archivos
 module.exports = {
-    query: (text, params) => pool.query(text, params),
-    connect: () => {
-      pool.connect()
-        .then(() => {
-          console.log('Conexión exitosa a la base de datos');
-        })
-        .catch((error) => {
-          console.error('Error al conectar a la base de datos', error);
-        });
-    },
-  };
+  query: (text, params) => pool.query(text, params),
+  connect: () => {
+    pool.connect()
+      .then(() => {
+        console.log('Conexión exitosa a la base de datos');
+      })
+      .catch((error) => {
+        console.error('Error al conectar a la base de datos', error);
+      });
+  },
+  end: () => {
+    pool.end()
+      .then(() => console.log('Conexión a la base de datos cerrada'))
+      .catch((error) => console.error('Error al cerrar la conexión', error));
+  },
+};
