eafef9a75888a0115d7902fd68e1d43d160397e8
jluchett <jluchett@hotmail.com>
2024-06-28 23:49:55 -0500

    Refactor createUser

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -2,10 +2,30 @@ const db = require('../../database/db')
 const bcrypt = require('bcrypt')
 
 const createUser = async (req, res) => {
-    const { id, name, password, role = "user", locked = false } = req.body
-    try {
-        
-    } catch () {
-        
-    }
+  const { id, name, password, role = 'ventas' } = req.body
+  try {
+    if (typeof id !== 'number') throw new Error('El Id debe ser un numero')
+    if (typeof name !== 'string') throw new Error('El nombre debe ser una cadena de texto')
+    if (name.length < 5) throw new Error('El nombre debe tener minimo 5 letras')
+    if (typeof password !== 'string') throw new Error('el password debe ser una cadena de texto')
+    if (password.length < 8) throw new Error('El password debe tener minimo 8 caracteres')
+
+    const usuario = await db.query('SELECT id FROM users WHERE id = $1', [id])
+    if (usuario) throw new Error('Usuario ya esta registrado')
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const insertQuery = 'INSERT INTO users (id, name, password, role) VALUES ($1, $2, $3, $4) RETURNING name'
+    const insertValues = [id, name, hashedPassword, role]
+    const result = await db.query(insertQuery, insertValues)
+    res.status(201).json({
+      mensaje: `Usuario ${result.rows[0].name} creado exitosamente`,
+      success: true
+    })
+  } catch (error) {
+    res.status(400).json({
+      mensaje: error.message,
+      success: false
+    })
+  }
 }
+module.exports = { createUser }
--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,5 @@
-//db.js
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg')
+require('dotenv').config()
 
 // Configuración de la conexión a la base de datos
 const pool = new Pool({
@@ -8,14 +7,14 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+  port: process.env.DB_PORT
+})
 
 // Manejo del cierre de la conexión
 pool.on('error', (err) => {
-  console.error('Error en el pool de conexiones:', err);
-  process.exit(-1);
-});
+  console.error('Error en el pool de conexiones:', err)
+  process.exit(-1)
+})
 
 // Exporta el objeto de conexión para usarlo en otros archivos
 module.exports = {
@@ -23,15 +22,15 @@ module.exports = {
   connect: () => {
     pool.connect()
       .then(() => {
-        console.log('Conexión exitosa a la base de datos');
+        console.log('Conexión exitosa a la base de datos')
       })
       .catch((error) => {
-        console.error('Error al conectar a la base de datos', error);
-      });
+        console.error('Error al conectar a la base de datos', error)
+      })
   },
   end: () => {
     pool.end()
       .then(() => console.log('Conexión a la base de datos cerrada'))
-      .catch((error) => console.error('Error al cerrar la conexión', error));
-  },
-};
+      .catch((error) => console.error('Error al cerrar la conexión', error))
+  }
+}
