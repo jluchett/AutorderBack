fee8332a40020589fb2a7712ed733c4bf392f882
jluchett <jluchett@hotmail.com>
2024-07-05 18:25:37 -0500

    agregado ruta para actualizar usuario

--- src/controllers/users/userController.js
+++ src/controllers/users/userController.js
@@ -2,10 +2,12 @@ const createUser = require('./createUser')
 const getUser = require('./getUser')
 const getUsers = require('./getUsers')
 const lockUser = require('./lockUser')
+const updateUser = require('./updateUser')
 
 module.exports = {
   createUser,
   getUser,
   getUsers,
-  lockUser
+  lockUser,
+  updateUser
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,10 +3,11 @@ const express = require('express')
 const router = express.Router()
 const controllerUser = require('../controllers/users/userController.js')
 
-// Rutas para la creación de usuarios
+// Rutas para administrar usuarios
 router.post('/create', controllerUser.createUser)
 router.get('/:id', controllerUser.getUser)
 router.get('/', controllerUser.getUsers)
 router.put('/locked/:id', controllerUser.lockUser)
+router.post('/update/:id', controllerUser.updateUser)
 
 module.exports = router
